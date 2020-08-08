e15be8eb06f7e43ca38cb90d9012caefcf457c29
Stephen Giebner <shgiebner21@gmail.com>
2020-08-08 17:34:57 -0400

    add PORT to env file

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
+
 const app = express();
+
+const PORT = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello, TPS Graduates...!')
 });
 
-app.listen(8080, () => {
-  console.log('Listening on port 8080')
+app.listen(PORT, () => {
+  console.log(`Listening on port ${ PORT }`)
 })
